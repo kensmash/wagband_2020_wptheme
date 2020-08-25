4ec7db7ad54864228bae140a24604156b6657a07
Ken <kensmash@gmail.com>
2020-08-25 17:57:28 -0400

    various things

--- template-parts/content-frontpage.php
+++ template-parts/content-frontpage.php
@@ -11,30 +11,38 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
-        <h1 class="entry-title">The Wag&rsquo;s Next Show!
+        <h2 class="entry-title">The Wag&rsquo;s Next Show!</h2>
     </header>
-
     <div class="entry-content">
         <?php // https://theeventscalendar.com/knowledgebase/k/using-tribe_get_events/
         global $post;
         
         $events = tribe_get_events( [ 'start_date' => 'now', 'posts_per_page' => 1 ] );
         
-        foreach ( $events as $post ) {
-        setup_postdata( $post );
-        
-        echo '<h4>' . $post->post_title . '</h4>';
-        echo ' ' . tribe_get_start_date( $post ) . ' ';
-       
-        echo tribe_event_featured_image( $post->ID, 'full' );
+        foreach ( $events as $post ):
+        setup_postdata( $post ); ?>
 
-        echo get_the_content();
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
+        <hr />
+
+        <?php 
+
+echo tribe_event_featured_image( $post->ID, 'full' );
+        
+        echo ' ' . tribe_get_start_date( $post ) . ' ';
+
+        
 
         echo tribe_get_venue( $post );
         echo tribe_get_address( $post );
         echo tribe_get_city( $post );
-  
-        } ?>
+
+        the_content();
+        endforeach; 
+
+        wp_reset_postdata(); 
+    ?>
 
     </div>
 
