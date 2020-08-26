95b4ced8b1d3f3700a6d00d45d2bbc8f1aeed1ee
Ken <kensmash@gmail.com>
2020-08-26 08:23:37 -0400

    fixins

--- sidebar.php
+++ sidebar.php
@@ -18,7 +18,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</div>
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-12">
 			<div class="content wilquote p-4">
 				<p>&ldquo;If the Beatles and the Cowsills got it on backstage at Ed Sullivan, The Wag would totally be their love child.&rdquo;
 					<i>-Wil Wheaton (Actor, Author)</i></p>
--- template-parts/content-frontpage.php
+++ template-parts/content-frontpage.php
@@ -9,10 +9,11 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class("mb-0"); ?>>
     <header class="entry-header">
         <h2 class="entry-title">The Wag&rsquo;s Next Show!</h2>
     </header>
+    <hr />
     <div class="entry-content">
         <?php // https://theeventscalendar.com/knowledgebase/k/using-tribe_get_events/
         global $post;
@@ -22,24 +23,22 @@
         foreach ( $events as $post ):
         setup_postdata( $post ); ?>
 
-        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
-        <hr />
+        <div class="home-event-meta mb-4 text-secondary">
+            <p><?php echo tribe_get_start_date( $post ) . ' - ' . tribe_get_end_time( $post ) ?></p>
+            <p><?php echo tribe_get_venue( $post ) ?></p>
+            <p><?php echo tribe_get_address( $post ) ?></p>
+            <p><?php echo tribe_get_city( $post ) . ' ' . tribe_get_state( $post )?></p>
+        </div>
 
-        <?php 
+        <?php the_content();
 
-echo tribe_event_featured_image( $post->ID, 'full' );
-        
-        echo ' ' . tribe_get_start_date( $post ) . ' ';
+         echo tribe_event_featured_image( $post->ID, 'full mx-auto d-block my-5 rounded' ); ?>
 
-        
+        <p class="font-weight-bold mb-1"><a href="<?php the_permalink(); ?>">See full show info</a></p>
 
-        echo tribe_get_venue( $post );
-        echo tribe_get_address( $post );
-        echo tribe_get_city( $post );
-
-        the_content();
-        endforeach; 
+        <?php endforeach; 
 
         wp_reset_postdata(); 
     ?>
