ca3e9486796f2198b976f704d53ebd0f28f63083
Ken <kensmash@gmail.com>
2020-08-26 21:47:21 -0400

    mo font styles

--- sidebar.php
+++ sidebar.php
@@ -13,14 +13,14 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 
-<aside id="secondary" class="widget-area pt-4">
+<aside id="secondary" class="widget-area mt-4">
 	<div class="row">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</div>
 	<div class="row">
 		<div class="col-12">
 			<div class="content wilquote p-4">
-				<p>&ldquo;If the Beatles and the Cowsills got it on backstage at Ed Sullivan, The Wag would totally be their love child.&rdquo;
+				<p class="pt-2">&ldquo;If the Beatles and the Cowsills got it on backstage at Ed Sullivan, The Wag would totally be their love child.&rdquo;
 					<i>-Wil Wheaton (Actor, Author)</i></p>
 			</div> <!-- /content -->
 		</div> <!-- /col-md-12 -->
