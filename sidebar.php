6debd47f017f0fa69c8bbf57f7942df9bacb24e5
Ken <kensmash@gmail.com>
2020-08-27 07:33:35 -0400

    update footer styles

--- sidebar.php
+++ sidebar.php
@@ -18,11 +18,14 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</div>
 	<div class="row">
-		<div class="col-12">
-			<div class="content wilquote p-4">
+		<div class="col-12 ">
+
+			<div class="wilquote p-4">
+				<h3>About The Wag</h3>
 				<p class="pt-2">&ldquo;If the Beatles and the Cowsills got it on backstage at Ed Sullivan, The Wag would totally be their love child.&rdquo;
 					<i>-Wil Wheaton (Actor, Author)</i></p>
-			</div> <!-- /content -->
+			</div>
+
 		</div> <!-- /col-md-12 -->
 	</div> <!-- /row -->
 </aside><!-- #secondary -->
