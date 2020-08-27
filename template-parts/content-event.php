a803ce976a7a557d7ad4b271a7ae453dbe1d7bb0
Ken <kensmash@gmail.com>
2020-08-27 09:17:37 -0400

    adjustments

--- template-parts/content-event.php
+++ template-parts/content-event.php
@@ -21,12 +21,15 @@ if (isset($_GET['tribe_event_display'])) {
 
 	<?php the_content(); ?>
 
-	<?php if ( $myTemplate !== 'past')  { ?>
+
 	<div class="px-2 mt-4">
-		<p>To book The Wag for a gig, private party or show, please contact us. The Wag can perform both electric & acoustic cover or original music for any occasion.</p>
-		<p><a href="/shows/past">View Past Shows</a>
+		<p>To book The Wag for a gig, private party or show, please <a href="<?php echo get_permalink( get_page_by_path( 'retailer-relief' ) )?>">contact us</a>. The Wag can perform both electric & acoustic cover or original music for any occasion.</p>
+		<?php if ( $myTemplate !== 'past')  { ?>
+		<p>
+			<a href="/shows/past">View Past Shows</a>
 			<p>
+				<?php } ?>
 	</div>
-	<?php } ?>
+
 
 </article>
